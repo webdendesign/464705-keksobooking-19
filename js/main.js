75a8c22ee5d86f1c145a3fba9845e63d3330f743
Den <fabri.din@yandex.ru>
2020-01-27 19:10:14 +0500

    create obeject

---
+++ js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// var count = 8;
+
+// var createElement = function () {
+//   var element = [];
+
+//   for (var i = 0; i < count; i++) {
+//     element.push({
+
+//     })
+//   }
+// // }
+
+// var map = document.querySelector('.map');
+// map.classList.remove('map--faded');
+
+// var template = document.querySelector('#template').content.querySelector('.map__pin');
+
+// var mapPins = document.querySelector('.map__pins');
+
+// for (var i = 0; i < usersAds.length; i++) {
+//   var userElement = template.cloneNode(true);
+
+//   userElement.querySelector('.map__pin').style.left = usersAds[i].location.x;
+//   userElement.querySelector('.map__pin').style.top = usersAds[i].location.y;
+
+
+//   userElement.querySelector('img').src = usersAds[i].author;
+
+//   userElement.querySelector('img').alt = usersAds[i].title;
+
+//   mapPins.appendChild(userElement);
+
+// }
